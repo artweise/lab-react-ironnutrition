da0f3e023c1700376b9fb4ae1b807fbd00861c22
artweise <art2wsxweise7ujm@gmail.com>
2023-02-26 18:55:42 +0200

    iteration 1 done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
+import foods from './foods.json';
+import { useState } from 'react';
 import './App.css';
+import { Card, Row, Col, Divider, Input, Button } from 'antd';
 
-function App() {
+const App = () => {
+  const [allDishes, setAllDishes] = useState(foods);
+
+  // return <div className="App"></div>;
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {allDishes.map((dish, index) => {
+        return (
+          <div key={index}>
+            <p>{dish.name}</p>
+            <img src={dish.image} className="dish-image" />
+          </div>
+        );
+      })}
     </div>
   );
-}
-
+};
 export default App;
+
+// inline style for width --> style={{ width: 150 }}
